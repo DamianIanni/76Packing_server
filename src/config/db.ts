6e7a8fab94a8a00310f256363eac7c9d5e47eef0
Damian Ianni <iannigabrieldamian@gmail.com>
2025-06-01 17:55:37 -0300

    trying to deploy in Render

--- src/config/db.ts
+++ src/config/db.ts
@@ -9,6 +9,7 @@ console.log(
 );
 
 const access: PoolOptions = {
+  port: Number(process.env.DB_PORT),
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
